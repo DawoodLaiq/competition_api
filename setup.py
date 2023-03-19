cd9b79de7ed5d23216d97e16c4fb469bf3286d35
DawoodLaiq <86498931+DawoodLaiq@users.noreply.github.com>
2023-03-19 05:36:04 +0500

    not alpha

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TeamQuiz",
-    version="0.0.4-alpha",
+    version="0.0.4",
     author="Dawood",
     description="A simple quiz program",
     packages=find_packages(),
